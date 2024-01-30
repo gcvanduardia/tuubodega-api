9ad9d6105d3eec1a9fb542759254e57b366b6c25
David Suarez <gcvanguardia@gmail.com>
2024-01-30 14:43:34 -0500

    feat: articulos admin

--- src/controllers/articulos.js
+++ src/controllers/articulos.js
@@ -1,5 +1,71 @@
 const sql = require("mssql");
 
+exports.searchAdmin = async (req, res) => {
+    const search = req.body.search || req.query.search;
+    const pageNumber = req.body.pageNumber || req.query.pageNumber || 1;
+    const pageSize = req.body.pageSize || req.query.pageSize || 12;
+    const order = req.body.order || req.query.order || 'masRelevante';
+
+    const request = new sql.Request();
+    let sql_searchInfo = '';
+    let sql_str = '';
+    if(search == undefined || search == ''){
+        if(pageNumber == 1){
+            sql_searchInfo = `
+            SELECT Resultados = COUNT(Id), PageZise = ${pageSize}
+            FROM vw_EncArticulosAdmin`;
+        }
+        sql_str = `
+            SELECT Id, Nombre, PrecioUnit, ImagenPrin, IdProveedor, Proveedor, ArticuloEn, CategoriaEn, ProveedorEn
+            FROM vw_EncArticulosAdmin
+            ORDER BY Id DESC
+            OFFSET ${pageSize} * (${pageNumber} - 1) ROWS
+            FETCH NEXT ${pageSize} ROWS ONLY;
+            
+            ${sql_searchInfo}`;
+    }else{
+        if(pageNumber == 1){
+            sql_searchInfo = `
+            SELECT Resultados = COUNT(Id), PageZise = ${pageSize}
+            FROM vw_EncArticulosAdmin
+            WHERE clave COLLATE Latin1_General_CI_AI LIKE '%${search}%'`;
+        }
+        let sqlOrder = '';
+        switch (order) {
+            case 'masRelevante':
+                sqlOrder = `ORDER BY CHARINDEX('${search}', clave COLLATE Latin1_General_CI_AI), nombre`;
+                break;
+            case 'menorPrecio':
+                sqlOrder = `ORDER BY PrecioUnit ASC`;
+                break;
+            case 'mayorPrecio':
+                sqlOrder = `ORDER BY PrecioUnit DESC`;
+                break;
+            default:
+                sqlOrder = `ORDER BY Id DESC`;
+                break;
+        }
+        sql_str = `
+            SELECT Id, Nombre, PrecioUnit, ImagenPrin, IdProveedor, Proveedor, ArticuloEn, CategoriaEn, ProveedorEn
+            FROM vw_EncArticulosAdmin
+            WHERE clave COLLATE Latin1_General_CI_AI LIKE '%${search}%'
+            ${sqlOrder}
+            OFFSET ${pageSize} * (${pageNumber} - 1) ROWS
+            FETCH NEXT ${pageSize} ROWS ONLY;
+            
+            ${sql_searchInfo}`;
+    }
+    console.log('Articulos search: ',sql_str);
+    request.query(sql_str)
+        .then((object) => {
+            res.status(200).json(object.recordsets);
+        })
+        .catch((err) => {
+            console.log('Articulos search: ',err);
+            res.status(500).send({ Error: true, Message: err });
+        });
+}
+
 exports.all = async (req, res) => {
     const request = new sql.Request();
     sql_str = 'SELECT * FROM vw_EncArticulos';
@@ -12,8 +78,6 @@ exports.all = async (req, res) => {
             res.status(500).send({ Error: true, Message: err });
         });
 }
-
-
 
 exports.search = async (req, res) => {
     const search = req.body.search || req.query.search;
@@ -33,7 +97,7 @@ exports.search = async (req, res) => {
         sql_str = `
             SELECT Id, Nombre, PrecioUnit, ImagenPrin
             FROM vw_EncArticulos
-            ORDER BY Id
+            ORDER BY Id DESC
             OFFSET ${pageSize} * (${pageNumber} - 1) ROWS
             FETCH NEXT ${pageSize} ROWS ONLY;
             
@@ -57,7 +121,7 @@ exports.search = async (req, res) => {
                 sqlOrder = `ORDER BY PrecioUnit DESC`;
                 break;
             default:
-                sqlOrder = `ORDER BY Id`;
+                sqlOrder = `ORDER BY Id DESC`;
                 break;
         }
         sql_str = `
--- src/routes/articulos.js
+++ src/routes/articulos.js
@@ -1,17 +1,23 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const articulosController = require('../controllers/articulos');
-const auth = require('../middleware/jwt');
+const articulosController = require("../controllers/articulos");
+const auth = require("../middleware/jwt");
 
-router.route('/all')
-    .get(articulosController.all)
+router.route("/all").get(articulosController.all);
 
-router.route('/search')
-    .post(articulosController.search)
-    .get(articulosController.search)
+router
+  .route("/search")
+  .post(articulosController.search)
+  .get(articulosController.search);
 
-router.route('/articulo')
-    .post(articulosController.article)
-    .get(articulosController.article)
+router
+  .route("/search-admin")
+  .post(auth,articulosController.searchAdmin)
+  .get(auth,articulosController.searchAdmin);
+
+router
+  .route("/articulo")
+  .post(articulosController.article)
+  .get(articulosController.article);
 
 module.exports = router;
